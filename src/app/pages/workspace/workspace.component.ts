e279bdadff1e6b135597b044c41bd9a4f66be396
Geovane-Araujo <geovane-araujo@outlook.com>
2024-12-03 21:30:50 -0300

    ajustes iniciais

--- src/app/pages/workspace/workspace.component.ts
+++ src/app/pages/workspace/workspace.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LoadingComponent} from "../../shared/loading/loading.component";
+import {BaseComponent} from "../../shared/common/base-component";
+import {TranslateService} from "../../shared/services/translate/translate.service";
 
 @Component({
   selector: 'app-workspace',
   standalone: true,
-  imports: [],
+  imports: [
+    LoadingComponent
+  ],
   templateUrl: './workspace.component.html',
   styleUrl: './workspace.component.scss'
 })
-export class WorkspaceComponent {
+export class WorkspaceComponent extends BaseComponent implements OnInit {
+
+  constructor(
+    public readonly translateService: TranslateService,
+  ) {
+    super();
+  }
+
+  ngOnInit(): void {
+  }
 
 }
